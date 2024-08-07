db2c6f81ecac9c270b98d5cba183951a4118d686
Sterling von Dehn <sterlingvondehn@gmail.com>
2024-08-07 12:22:40 -0700

    updates

--- py/lx_all.py
+++ py/lx_all.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import cartopy.crs as ccrs
 import geopandas as gpd
+import matplotlib.gridspec as gridspec
 
 max_radius = 1000 #define a max radius
 #loads data for all fire centers and combines them for all of BC
@@ -61,4 +62,73 @@ ax.scatter(np.nan,np.nan, marker='o', color='black', label=f'Both sensors missed
 plt.title('LCFs ignition points colored by sensor detection combinations',fontsize=14)
 plt.legend(fontsize=14)
 plt.tight_layout()
-plt.savefig(f'plots/{max_radius}data/LCFs/{max_radius}m-strike-detection.png')    
+plt.savefig(f'plots/{max_radius}data/LCFs/{max_radius}m-strike-detection.png')
+plt.clf()
+
+# zones = ['BC','PG', 'NW', 'Cariboo', 'Coast', 'Kam', 'SE']
+# zone_vals_both = [len(data_bc[0]),len(data_pg[0]),len(data_nw[0]),len(data_car[0]), len(data_coast[0]), len(data_kam[0]), len(data_se[0])]
+# zone_vals_aem = [len(data_bc[1]),len(data_pg[1]),len(data_nw[1]),len(data_car[1]), len(data_coast[1]), len(data_kam[1]), len(data_se[1])]
+# zone_vals_cldn = [len(data_bc[2]),len(data_pg[2]),len(data_nw[2]),len(data_car[2]), len(data_coast[2]), len(data_kam[2]), len(data_se[2])]
+# zone_vals_none = [len(data_bc[3]),len(data_pg[3]),len(data_nw[3]),len(data_car[3]), len(data_coast[3]), len(data_kam[3]), len(data_se[3])]
+# colors = ['red', 'blue', 'green', 'purple', 'orange', 'cyan', 'magenta']
+
+# fig, axs = plt.subplots(2,2,figsize=(15, 15))
+# axs[0,0].set_title('Both sensors detected')
+# axs[0,0].set_ylabel('# of fires')
+# axs[0,0].bar(zones,zone_vals_both,color=colors)
+# axs[0,1].set_title('Just AEM detected')
+# axs[0,1].set_ylabel('# of fires')
+# axs[0,1].bar(zones,zone_vals_aem,color=colors)
+# axs[1,0].set_title('Just CLDN detected')
+# axs[1,0].set_ylabel('# of fires')
+# axs[1,0].bar(zones,zone_vals_cldn,color=colors)
+# axs[1,1].set_title('Both sensors missed')
+# axs[1,1].set_ylabel('# of fires')
+# axs[1,1].bar(zones,zone_vals_none,color=colors)
+# plt.show()
+
+fig = plt.figure(figsize=(12, 16))
+
+gs = gridspec.GridSpec(4, 2, height_ratios=[1, 1, 1, 1], width_ratios=[1, 1], hspace=0.4, wspace=0.3)
+ax1 = plt.subplot(gs[0, 0])
+ax2 = plt.subplot(gs[0, 1])
+ax3 = plt.subplot(gs[1, 0])
+ax4 = plt.subplot(gs[1, 1])
+ax5 = plt.subplot(gs[2, 0])
+ax6 = plt.subplot(gs[2, 1])
+ax7 = plt.subplot(gs[3, 0:2])
+
+names = ['Hit', 'Only AEM', 'Only CLDN', 'Miss']
+bc = [len(data_bc[0]), len(data_bc[1]), len(data_bc[2]), len(data_bc[3])]
+pg = [len(data_pg[0]), len(data_pg[1]), len(data_pg[2]), len(data_pg[3])]
+nw = [len(data_nw[0]), len(data_nw[1]), len(data_nw[2]), len(data_nw[3])]
+car = [len(data_car[0]), len(data_car[1]), len(data_car[2]), len(data_car[3])]
+coast = [len(data_coast[0]), len(data_coast[1]), len(data_coast[2]), len(data_coast[3])]
+kam = [len(data_kam[0]), len(data_kam[1]), len(data_kam[2]), len(data_kam[3])]
+se = [len(data_se[0]), len(data_se[1]), len(data_se[2]), len(data_se[3])]
+colors = ['green', 'orange', 'red', 'black']
+
+ax1.set_title('South East')
+ax1.set_ylabel('# of fires')
+ax1.bar(names,se,color=colors)
+ax2.set_title('Prince George')
+ax2.set_ylabel('# of fires')
+ax2.bar(names,pg,color=colors)
+ax3.set_title('North West')
+ax3.set_ylabel('# of fires')
+ax3.bar(names,nw,color=colors)
+ax4.set_title('Cariboo')
+ax4.set_ylabel('# of fires')
+ax4.bar(names,car,color=colors)
+ax5.set_title('Coast')
+ax5.set_ylabel('# of fires')
+ax5.bar(names,coast,color=colors)
+ax6.set_title('Kamloops')
+ax6.set_ylabel('# of fires')
+ax6.bar(names,kam,color=colors)
+ax7.set_title('BC')
+ax7.set_ylabel('# of fires')
+ax7.bar(names,bc,color=colors)
+
+plt.tight_layout()
+plt.savefig(f'plots/{max_radius}data/LCFs/{max_radius}m-hist.png')
